7b3efc83995dd1a36ed8d2e757709b9e839b1688
RayugaLdrag <akc64016@gmail.com>
2024-07-04 10:11:39 +0530

    Resume updated

--- src/sections/Hero/Hero.jsx
+++ src/sections/Hero/Hero.jsx
@@ -8,7 +8,7 @@ import githubLight from "../../assets/github-light.svg";
 import githubDark from "../../assets/github-dark.svg";
 import linkedinLight from "../../assets/linkedin-light.svg";
 import linkedinDark from "../../assets/linkedin-dark.svg";
-import CV from "../../assets/cv.pdf";
+import CV from "../../assets/Resume.pdf";
 import { useTheme } from "../../common/ThemeContext";
 
 function Hero() {
