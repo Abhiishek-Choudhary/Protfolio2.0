286bca41ceb3c72a08a690f0e2bd07e01caae02c
RayugaLdrag <akc64016@gmail.com>
2025-06-20 16:39:27 +0530

    final commit

--- src/sections/Hero/Hero.jsx
+++ src/sections/Hero/Hero.jsx
@@ -1,5 +1,8 @@
 import styles from "./HeroStyles.module.css";
+<<<<<<< HEAD
 import heroImg from "../../assets/hero-img.png";
+=======
+>>>>>>> 14d1c3c (final commit)
 import sun from "../../assets/sun.svg";
 import moon from "../../assets/moon.svg";
 import twitterLight from "../../assets/twitter-light.svg";
@@ -8,8 +11,14 @@ import githubLight from "../../assets/github-light.svg";
 import githubDark from "../../assets/github-dark.svg";
 import linkedinLight from "../../assets/linkedin-light.svg";
 import linkedinDark from "../../assets/linkedin-dark.svg";
+<<<<<<< HEAD
 import CV from "../../assets/Resume.pdf";
 import { useTheme } from "../../common/ThemeContext";
+=======
+import { useTheme } from "../../common/ThemeContext";
+import profile from '../../../public/profile.jpeg'
+import resume from '../../../public/Abhishek_Choudhary_resume.pdf'
+>>>>>>> 14d1c3c (final commit)
 
 function Hero() {
   const { theme, toggleTheme } = useTheme();
@@ -24,7 +33,11 @@ function Hero() {
       <div className={styles.colorModeContainer}>
         <img
           className={styles.hero}
+<<<<<<< HEAD
           src={heroImg}
+=======
+          src={profile}
+>>>>>>> 14d1c3c (final commit)
           alt="Profile picture of Abhishek Choudhary"
         />
         <img
@@ -56,7 +69,11 @@ function Hero() {
           I am Abhishek Choudhary a full stack web developer from raipur
            and this is my protfolio.
         </p>
+<<<<<<< HEAD
         <a href={CV} download>
+=======
+        <a href={resume} download>
+>>>>>>> 14d1c3c (final commit)
           <button className="hover">Resume</button>
         </a>
       </div>
--- src/sections/Projects/Projects.jsx
+++ src/sections/Projects/Projects.jsx
@@ -1,9 +1,17 @@
 import styles from "./ProjectStyles.module.css";
+<<<<<<< HEAD
 import viber from "../../assets/viberr.png";
 import freshBurger from "../../assets/fresh-burger.png";
 import Hipsster from "../../assets/hipsster.png"
 import fitlift from "../../assets/fitlift.png"
 import ProjectCard from "../../common/ProjectCard";
+=======
+import ProjectCard from "../../common/ProjectCard";
+import movie from "../../assets/movie.png"
+import music from "../../assets/music.png"
+import chat from "../../assets/chat.png"
+import whatgpt from "../../assets/whatgpt.png"
+>>>>>>> 14d1c3c (final commit)
 
 function Projects() {
   return (
@@ -11,6 +19,7 @@ function Projects() {
       <h1 className="sectionTitle">Projects</h1>
       <div className={styles.projectsContainer}>
         <ProjectCard
+<<<<<<< HEAD
           src={viber}
           link="https://github.com/Abhiishek-Choudhary/Spotify"
           h3="Viberr"
@@ -33,6 +42,30 @@ function Projects() {
           link="https://github.com/Abhiishek-Choudhary/WhatGPT3"
           h3="Whatgpt3"
           p="Frontend App"
+=======
+          src={music}
+          link="https://spotify2-0-opal.vercel.app/"
+          h3="Spotify-M"
+          p="Music App"
+        />
+        <ProjectCard
+          src={movie}
+          link="https://hianime7.netlify.app/"
+          h3="Hianime"
+          p="Movie App"
+        />
+        <ProjectCard
+          src={chat}
+          link="https://chat-app-ten-alpha-25.vercel.app/"
+          h3="You Chat"
+          p="Chat App"
+        />
+        <ProjectCard
+          src={whatgpt}
+          link="https://whatgpt4.netlify.app/"
+          h3="Whatgpt3"
+          p="AI App"
+>>>>>>> 14d1c3c (final commit)
         />
       </div>
     </section>
