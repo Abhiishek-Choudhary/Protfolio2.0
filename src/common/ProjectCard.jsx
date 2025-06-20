b14ecf6c21eac33aed0d4677c9a0fa9c5f1bc413
RayugaLdrag <akc64016@gmail.com>
2025-06-20 17:08:52 +0530

    projectCard

--- src/common/ProjectCard.jsx
+++ src/common/ProjectCard.jsx
@@ -3,11 +3,7 @@ import React from "react";
 function ProjectCard({ src, link, h3, p }) {
   return (
     <a href={link}>
-<<<<<<< HEAD
-      <img className="hover" src={src} alt={`${h3} logo`} />
-=======
       <img style={{height:180}} className="hover" src={src} alt={`${h3} logo`} />
->>>>>>> 14d1c3c (final commit)
       <h3>{h3}</h3>
       <p>{p}</p>
     </a>
