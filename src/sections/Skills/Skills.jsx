a0a740fa257236a0c181be010fc6c4a62808b3f0
Abhishek Choudhary <108070718+Abhiishek-Choudhary@users.noreply.github.com>
2025-07-23 10:45:42 +0530

    Update Skills.jsx

--- src/sections/Skills/Skills.jsx
+++ src/sections/Skills/Skills.jsx
@@ -8,26 +8,30 @@ function Skills() {
     <section id='skills' className={styles.container}>
         <h1 className='sectionTitle'>Skills</h1>
         <div className={styles.skillList}>
-           <SkillList src={checkMarkIcon} skill="HTML" />
-           <SkillList src={checkMarkIcon} skill="CSS" />
-           <SkillList src={checkMarkIcon} skill="JAVASCRIPT" />
-           <SkillList src={checkMarkIcon} skill="BOOTSTRAP" />
-           <SkillList src={checkMarkIcon} skill="NODE" />
-        </div>
-        <hr />
-        <div className={styles.skillList}>
            <SkillList src={checkMarkIcon} skill="REACT" />
-           <SkillList src={checkMarkIcon} skill="EXPRESS" />
-           <SkillList src={checkMarkIcon} skill="TYPESCRIPT" />
+           <SkillList src={checkMarkIcon} skill="NODE" />
            <SkillList src={checkMarkIcon} skill="MONGODB" />
+           <SkillList src={checkMarkIcon} skill="EXPRESS" />
            <SkillList src={checkMarkIcon} skill="TAILWIND" />
         </div>
         <hr />
         <div className={styles.skillList}>
-           <SkillList src={checkMarkIcon} skill="C" />
+           <SkillList src={checkMarkIcon} skill="NEXT" />
+           <SkillList src={checkMarkIcon} skill="REDUX" />
+           <SkillList src={checkMarkIcon} skill="TYPESCRIPT" />
+           <SkillList src={checkMarkIcon} skill="PAYMENT GATEWAY" />
+           <SkillList src={checkMarkIcon} skill="ZOD" />
+        </div>
+        <hr />
+        <div className={styles.skillList}>
            <SkillList src={checkMarkIcon} skill="CPP" />
            <SkillList src={checkMarkIcon} skill="JAVA" />
            <SkillList src={checkMarkIcon} skill="PYTHON" />
+        </div>
+      <div className={styles.skillList}>
+           <SkillList src={checkMarkIcon} skill="DOCKER" />
+           <SkillList src={checkMarkIcon} skill="KUBERNETES" />
+           <SkillList src={checkMarkIcon} skill="GITHUB ACTIONS" />
         </div>
     </section>
   )
